7c8c01c969b84e9659834c4e5c8a6f0653415e9f
Bjorn Wickstrom <bjorn@billo.systems>
2017-05-28 18:15:48 -0500

    Re-orged test ns.

---
+++ test/systems/billo/test/datagram/core.clj
@@ -0,0 +1,7 @@
+(ns systems.billo.test.datagram.core
+  (:require [clojure.test :refer :all]
+            [systems.billo.datagram.core :as datagram]))
+
+(deftest a-test
+  (testing "Placeholder ..."
+    (is (zero? 0))))
