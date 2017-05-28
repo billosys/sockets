79f9121e57024c41a9470c857f717e2ca7de7976
Duncan McGreggor <oubiwann@gmail.com>
2017-05-28 18:16:42 -0500

    Added core.async.

--- dev-resources/src/sockets/dev.clj
+++ dev-resources/src/sockets/dev.clj
@@ -1,5 +1,6 @@
 (ns sockets.dev
   (:require
+    [clojure.core.async :as async]
     [clojure.java.shell :as shell]
     [clojure.tools.namespace.repl :as repl]
     [quote-server.quotes :as quotes]
