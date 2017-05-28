87ee4ab94bb8aefe82f641610ec4780ec46e0462
Bjorn Wickstrom <bjorn@billo.systems>
2017-05-28 18:09:14 -0500

    Added inet-address dep.

--- project.clj
+++ project.clj
@@ -6,7 +6,8 @@
     :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [
     [org.clojure/clojure "1.8.0"]
-    [potemkin "0.4.3"]]
+    [potemkin "0.4.3"]
+    [systems.billo/inet-address "0.1.0"]]
   :profiles {
     :docs {
       :dependencies [
