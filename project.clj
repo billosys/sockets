d318b4d08e758ea5e9d50ef1f11493426ab515a5
Døkkarr Hirðisson <dokkarr@billo.systems>
2017-05-29 23:58:22 -0500

    Bumped for release.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject systems.billo/sockets "0.1.0-SNAPSHOT"
+(defproject systems.billo/sockets "0.1.0"
   :description "A Clojure wrapper for the family of Java Socket classes"
   :url "https://github.com/billosys/sockets"
   :license {
